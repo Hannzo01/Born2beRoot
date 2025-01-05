b4d38c355f98e8f72001ea2f76156e7266f239d9
Kenza Mzouri <kemzouri@c3r5p1.1337.ma>
2025-01-05 14:42:00 +0100

    done

--- monitoring.sh
+++ monitoring.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-US=$(df -BM | grep '/dev/mapper\|sda1' | awk '{us += $3} END {print us}')
-TS=$(df -BG | grep '/dev/mapper\|sda1' | awk '{ts += $2} END {print ts "Gb"}')
+US=$(df -BM | grep /dev/mapper | awk '{us += $3} END {print us}')
+TS=$(df -BG | grep /dev/mapper |sda1' | awk '{ts += $2} END {print ts "Gb"}')
 PS=$(df -BM | grep '/mapper' | awk '{us += $3} {ts += $2} END {printf("(%d%%)\n", us/ts*100)}')
 wall "
 	#Architecture:	$(uname -a)
@@ -9,7 +9,7 @@ wall "
 	#vCPU: $(lscpu | grep "^CPU(s)" | awk '{print $2}')
 	#Memory usage: $(free -m | grep "Mem:" | awk '{printf("%d/%dMB (%.2f%%)",$3, $2, ($3/$2)*100)}')
 	#Disk Usage:	$US/$TS  $PS
-	#CPU Load:	$(vmstat 1 2 | awk 'NR==4 {printf("%d%%",100-$15)}')
+	#CPU Load:	$(vmstat 1 2 | awk 'NR==4 {printf("%.2f%%",100-$15)}')
 	#Last boot:	$(who -b | awk '{print $3, $4}')
 	#LVM use:	$(lsblk | grep -q "lvm" && echo "yes" || echo "no")
 	#Connections TCP: $(ss -tH state established | wc -l) "ESTABLISHED" 
